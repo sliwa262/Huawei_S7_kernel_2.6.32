ca6825ecc53fa16be6d6e0344e10e795a7f4e876
asturel <astur3l@gmail.com>
2012-09-08 22:26:43 +0200

    changed adp size, gpu idle timeout

--- arch/arm/mach-msm/board-qsd8x50-s7.c
+++ arch/arm/mach-msm/board-qsd8x50-s7.c
@@ -204,8 +204,8 @@
 
 #define SMEM_SPINLOCK_I2C	"S:6"
 
-//#define MSM_PMEM_ADSP_SIZE	0x2B96000
-#define MSM_PMEM_ADSP_SIZE  0x1100000
+#define MSM_PMEM_ADSP_SIZE	0x2B96000
+//#define MSM_PMEM_ADSP_SIZE  	0x1100000
 
 
 #ifdef CONFIG_MSM_HDMI
@@ -1735,12 +1735,13 @@ static struct kgsl_device_platform_data kgsl_3d0_pdata = {
 	    {
 		.gpu_freq = 128000000,
 		.bus_freq = 128000000,
+		.io_fraction = 0,
 	    },
 	},
 	.init_level = 0,
 	.num_levels = 1,
 	.set_grp_async = NULL,
-	.idle_timeout = HZ/5,
+	.idle_timeout = HZ/20,
 	.nap_allowed = true,
     },
     .clk = {
